c86b093e8a592de8b39023b610fa9a5d3c9abc67
Benedetta52 <91663828+Benedetta52@users.noreply.github.com>
2021-11-14 12:58:59 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,20 @@
 using namespace std;
 
 int main() {
+    int n=0, n1=1, n2=1, n3;
+    cin >> n;
+
+    if (n<2) {
+        cout << "errore" << endl;
+    } else {
+        cout << "1" << endl;
+        for (int i=0; i<=(n-2); i++) {
+            n3 = n1 + n2;
+            n1 = n2;
+            n2 = n3;
+            cout << n3 << endl;
+        }
+
+    }
    return 0;
 }
