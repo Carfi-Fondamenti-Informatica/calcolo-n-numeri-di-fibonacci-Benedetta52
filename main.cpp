f256ff79aee715a62e4e3f720b33d19916cea16e
Benedetta52 <91663828+Benedetta52@users.noreply.github.com>
2021-11-14 13:02:26 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
+
 using namespace std;
 
 int main() {
-    int n=0, n1=1, n2=1, n3;
+    int n=0, primo=0, secondo=1, terzo;
     cin >> n;
 
     if (n<2) {
@@ -10,10 +11,10 @@ int main() {
     } else {
         cout << "1" << endl;
         for (int i=0; i<=(n-2); i++) {
-            n3 = n1 + n2;
-            n1 = n2;
-            n2 = n3;
-            cout << n3 << endl;
+            terzo = primo + secondo;
+            primo = secondo;
+            secondo = terzo;
+            cout << terzo << endl;
         }
 
     }
